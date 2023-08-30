f01eba893cb44c7784b1fd1be77572aa87553eb3
Jakesoso <1343938313@qq.com>
2023-08-30 14:27:39 +0800

    test: Update example usage

---
+++ tiktok_helper/main.py
@@ -0,0 +1,15 @@
+from core.livestream import live_info
+from core.login import qrcode
+
+'''
+    this is an example usage.
+'''
+
+if __name__ == '__main__':
+    # login method
+    qrcode.login()
+
+    # get livestream like count
+    live_id = '336586944484'
+    like_count = live_info.get_like_count(live_id)
+    print(like_count)
